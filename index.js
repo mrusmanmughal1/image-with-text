388260da6e255f4336fb32bd5176a538470a9279
usman <mrusmanmughal1@gmail.com>
2023-09-18 19:49:52 +0500

    images added

--- index.js
+++ index.js
@@ -2,26 +2,23 @@ const express = require("express");
 const app = express();
 const Jimp = require("jimp");
 const port = 4550;
-const inputImagePath = "check2.jpg";
+const inputImagePath = "2.jpeg";
 const outputImagePath = "outputtt.jpg";
-const text = "ABCD";
-const fontFamily ="./d.fnt";
+const text = "AAAA";
+const fontFamily ="../250.fnt";
 
 // Define a larger font size
 Jimp.loadFont(fontFamily) // You can use a different Jimp font if needed
   .then((font) => {
     return Jimp.read(inputImagePath).then((image) => {
       // Create a new Jimp image for the text
-      const textImage = new Jimp(200, 100); // Adjust the width and height as needed
-      textImage.print(font, 55, 35, text); // Add text to the textImage
+      const textImage = new Jimp(700, 300); // Adjust the width and height as needed
+      textImage.print(font, 0, 0, text); // Add text to the textImage
       textImage.rotate(90); // Rotate the text by 45 degrees
-      textImage.resize(1200,1400)
 
       // Add a red color to the text (RGBA format)
       textImage.color([
-        { apply: "hue", params: [-90] },
-        { apply: "lighten", params: [50] },
-        { apply: "xor", params: ["#FFFFFF"]}
+        { apply: "xor", params: ["#D3D0AD"]}
       ]);
 
       // Apply a text shadow by cloning the text with a slight offset and color
@@ -29,9 +26,9 @@ Jimp.loadFont(fontFamily) // You can use a different Jimp font if needed
       const shadowColor = Jimp.rgbaToInt(0, 0, 25, 100);  
       const shadowImage = textImage.clone().color([
      
-        { apply: "xor", params: ["#ff0000"] }
+        { apply: "xor", params: ["#F3E020"] }
       ]);
-    //   textImage.clone().color([{ apply: "darken", params: [shadowColor, 100] }]);
+      textImage.clone().color([{ apply: "darken", params: [shadowColor, 100] }]);
 
       // Calculate the position to center the rotated textImage
       const textX = (image.bitmap.width - textImage.bitmap.width) / 2;
@@ -43,7 +40,7 @@ Jimp.loadFont(fontFamily) // You can use a different Jimp font if needed
             image.composite(shadowImage, shadowX, shadowY);
       // Composite the rotated textImage onto the original image
       image.composite(textImage, textX, textY);
-
+console.log(image.getWidth() , image.getHeight())
       return image;
     });
   })
