08b35a26b5906cd41e39b8ea02814f1af041ee86
usman <mrusmanmughal1@gmail.com>
2023-09-19 13:01:47 +0500

    img api end point

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 4550;
 const inputImagePath = "2.jpeg";
 const outputImagePath = "outputtt.jpg";
 const text = "AAAA";
-const fontFamily ="../250.fnt";
+const fontFamily = "../250.fnt";
 
 // Define a larger font size
 Jimp.loadFont(fontFamily) // You can use a different Jimp font if needed
@@ -17,30 +17,29 @@ Jimp.loadFont(fontFamily) // You can use a different Jimp font if needed
       textImage.rotate(90); // Rotate the text by 45 degrees
 
       // Add a red color to the text (RGBA format)
-      textImage.color([
-        { apply: "xor", params: ["#D3D0AD"]}
-      ]);
+      textImage.color([{ apply: "xor", params: ["#D3D0AD"] }]);
 
       // Apply a text shadow by cloning the text with a slight offset and color
       const shadowOffset = -7; // Adjust the shadow offset as needed
-      const shadowColor = Jimp.rgbaToInt(0, 0, 25, 100);  
-      const shadowImage = textImage.clone().color([
-     
-        { apply: "xor", params: ["#F3E020"] }
-      ]);
-      textImage.clone().color([{ apply: "darken", params: [shadowColor, 100] }]);
+      const shadowColor = Jimp.rgbaToInt(0, 0, 25, 100);
+      const shadowImage = textImage
+        .clone()
+        .color([{ apply: "xor", params: ["#F3E020"] }]);
+      textImage
+        .clone()
+        .color([{ apply: "darken", params: [shadowColor, 100] }]);
 
       // Calculate the position to center the rotated textImage
-      const textX = (image.bitmap.width - textImage.bitmap.width) / 2;
+      const textX = (image.bitmap.width - textImage.bitmap.width) / 1.5;
       const textY = (image.bitmap.height - textImage.bitmap.height) / 2;
       const shadowX = textX + shadowOffset;
       const shadowY = textY + shadowOffset;
 
-            // Composite the shadowImage onto the original image
-            image.composite(shadowImage, shadowX, shadowY);
+      // Composite the shadowImage onto the original image
+      image.composite(shadowImage, shadowX, shadowY);
       // Composite the rotated textImage onto the original image
       image.composite(textImage, textX, textY);
-console.log(image.getWidth() , image.getHeight())
+      console.log(image.getWidth(), image.getHeight());
       return image;
     });
   })
@@ -57,12 +56,14 @@ console.log(image.getWidth() , image.getHeight())
     console.error(err);
   });
 
-
 // Define an endpoint
-app.use('/', (req, res) => {
-  res.send('This is my custom endpoint!');
+app.get('/img', (req, res) => {
+  // Construct the URL of the generated image
+  const imageUrl = `http://localhost:${port}/${outputImagePath}`;
+  
+  // Send the image URL as a response
+  res.json({ imageUrl });
 });
-
 
 app.listen(port, () => {
   console.log(`App is listening to the port ${port}`);
